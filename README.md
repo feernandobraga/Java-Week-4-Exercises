78861bc5de0993953315f8a5826ef528e6a1d823
feernandobraga <fernando.bragga@gmail.com>
2019-08-21 13:01:32 +1000

    Exercise one tweaked

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Java-Week-4-Exercises
 Week 4 - More Java Exercises (arrays & bubble sort)
+
+- Exercise 1 
+    * Write a Java program that stores the names of your 10 favourite songs in an array of strings. 
+    Also store the artist's name in a second array of strings. 
+    Output a list of the songs and artists.
--- src/ExerciseOne.java
+++ src/ExerciseOne.java
@@ -1,6 +1,5 @@
-//Exercise 1 Write a Java program that stores the names of your 10 favourite songs in
-//        an array of strings. Also store the artist's name in a second array of
-//        strings. Output a list of the songs and artists.
+//why is it not working?
+
 import java.util.Scanner;
 public class ExerciseOne {
 
