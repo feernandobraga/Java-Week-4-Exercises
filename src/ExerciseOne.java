1f00c968f4baeb2c3026d60632762247e981a036
feernandobraga <fernando.bragga@gmail.com>
2019-08-21 12:52:38 +1000

    Exercise one tweaked

--- src/ExerciseOne.java
+++ src/ExerciseOne.java
@@ -1,2 +1,31 @@
+//Exercise 1 Write a Java program that stores the names of your 10 favourite songs in
+//        an array of strings. Also store the artist's name in a second array of
+//        strings. Output a list of the songs and artists.
+import java.util.Scanner;
 public class ExerciseOne {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String[] songs = new String[10];
+        String[] artists = new String[10];
+
+        for (int i = 0; i < songs.length; i++) {
+            System.out.println("\nPlease enter a favorite song:");
+            songs[i] = userInput.nextLine();
+            System.out.println("\nNow enter your favorite artist:");
+            artists[i] = userInput.nextLine();
+        }
+
+        System.out.println("\nYour favorite songs are:");
+        for (int i = 0; i < songs.length; i++) {
+            System.out.print(songs[0] + ", ");
+        }
+
+        System.out.println("\nYour favorite artits are:");
+        for (int i = 0; i < artists.length; i++) {
+            System.out.print(artists[0] + ", ");
+        }
+
+    }
+
 }
